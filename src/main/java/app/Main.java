831f777b065cd6c64ad93e159ac4f0b54c387e2c
AerrowV <businessasimk@gmail.com>
2024-10-23 10:36:02 +0200

    Added routes to main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,29 @@
 package app;
 
+import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
 public class Main {
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "lifehack";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Initializing Javalin and Jetty webserver
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+            config.staticFiles.add("/templates");
+        }).start(7070);
+
+        // Routing
+        app.get("/", ctx -> ctx.render("index.html"));
+
     }
 }
